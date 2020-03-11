ed193437397d4f8ae664fa73257f16da0ceb9400
truong.nguyen <truong.nguyen@persol.co.jp>
2020-03-11 11:11:03 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Todo List with Mobx</h1>
+      <div className="form-group">
+        <label>Add Item</label>
+        <input type="text" className="form-control" name="" id="itemInput" />
+      </div>
+      <div className="buttonContainer">
+        <button className="btn btn-primary">Add To List</button>
+        <button className="btn btn-danger">Clear Todo List</button>
+      </div>
+      <h3>Todo List</h3>
+      <ul id="todoList">
+        <li className="completed well">
+          <label>dinner</label>
+          <button className="btn btn-success">Complete</button>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </div>
   );
 }
